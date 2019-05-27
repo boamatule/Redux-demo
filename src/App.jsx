08182a0630ce4fd681d0eda5b2f6ded1f6f755cc
Boa Matule <boa.matule@gmail.com>
2019-05-27 18:11:02 +0200

    Creates submitForm and its imputs for the signInUser.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,62 @@
-import React from 'react';
+import React, { Component }from 'react';
 import { connect } from 'react-redux';
 import {Container, Header, Button, Input } from 'semantic-ui-react';
 
 
 
 
-const App = (props) => {
-  return (
-    <>
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    };
+  };
+
+  inputHandler() {
+
+  }
+
+  submitForm() {
+    
+  }
+
+  render() {
+    return (
+      <>
       <Container>
-        <Header as='h1'>{props.state.greeting}</Header>
+        <Header as='h1'>{this.props.state.greeting}</Header>
         <Input
           placeholder='New greeting'
-          onBlur={(event) => props.dispatch({ type: 'PROPOSE_GREETING',
+          onBlur={(event) => this.props.dispatch({ type: 'PROPOSE_GREETING',
         greeting: event.target.value })}
         />
 
         <Button
           primary
-          onClick={() => props.dispatch({ type: 'CHANGE_GREETING',
+          onClick={() => this.props.dispatch({ type: 'CHANGE_GREETING',
           greeting: 'Hello Venus' })}
         >
           Change greeting
         </Button>
       </Container>
+      
+      <>
+        <imput id='email' onChange={this.imputHandler.bind(this)}></imput>
+        <imput id='password' onChange={this.imputHandler.bind(this)}></imput>
+        <button onClick={this.submitForm}></button>
+      </>
     </>
-  );
+    )
+  }
 }
 
-  const mapStateToProps = (state) => {
-    return {
-      state: state 
-    }
+
+const mapStateToProps = (state) => {
+  return {
+    state: state 
   }
+}
 
 export default connect(mapStateToProps) (App)
